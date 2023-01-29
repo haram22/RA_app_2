158ae4cf52b3a7a0ff5e55f07cea329cac54009c
haram22 <97924765+haram22@users.noreply.github.com>
2023-01-29 16:38:54 +0900

    chage calendar

--- lib/home/home_manager.dart
+++ lib/home/home_manager.dart
@@ -122,8 +122,16 @@ class _Home_mState extends State<Home_m> {
                         digitsColor: Colors.black,
                         selectedBackgroundColor: const Color(0xff316A62),
                         selectedDigitColor: Colors.white,
-                        weekdays: ["Mo", "Tu", "We", "Th", "Fr"],
-                        daysInWeek: 5,
+                        weekdays: [
+                          "Mon",
+                          "Tue",
+                          "Wen",
+                          "Thu",
+                          "Fri",
+                          "Sat",
+                          "Sun"
+                        ],
+                        daysInWeek: 7,
                       ),
                     ),
                   ],
